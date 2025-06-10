53299f99362fc283e4a81a8917647584178a7399
Jeevan Baabu <jeevanbaabu03@gmail.com>
2025-06-10 22:57:58 +0400

    Home Page changed

--- nextext/src/app/page.tsx
+++ nextext/src/app/page.tsx
@@ -8,8 +8,20 @@ export default function Home() {
  
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white p-4">
-      <div className="w-full max-w-md text-center">
+    
+    <div className=" bg-black  text-white p-4">
+    <div className=" ">
+      <ul> 
+        <li>
+          <a>Sign Up</a>
+        </li>
+       <li>
+          <a>Login</a>
+        </li>
+      </ul>
+    </div>
+    
+      <div className="min-h-screen flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold mb-8">Welcome to NexText</h1>
         
         
