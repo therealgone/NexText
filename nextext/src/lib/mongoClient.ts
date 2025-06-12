38d4e6118d643ad846b7eb623f5229570521bba1
Jeevan Baabu <jeevanbaabu03@gmail.com>
2025-06-12 17:22:29 +0400

    FIX V3

--- nextext/src/lib/mongoClient.ts
+++ nextext/src/lib/mongoClient.ts
@@ -5,7 +5,11 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI;
-const options = {};
+const options = {
+  maxPoolSize: 10,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+};
 
 let client;
 let clientPromise: Promise<MongoClient>;
